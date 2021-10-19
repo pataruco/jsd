c78b18d6ea0db455473ca94653ed3f4548ae9430
Pedro Martin <pataruco@gmail.com>
2021-10-19 20:44:42 +0100

    Add bonuses and resources

--- web/src/pages/weeks/1.tsx
+++ web/src/pages/weeks/1.tsx
@@ -1,66 +1,79 @@
 import Week from '../../components/week';
 
-// const resources = [
-//   ['Intro to HTML', 'https://developer.mozilla.org/en-US/docs/Web/HTML'],
-//   [
-//     'Intro to HTML 5',
-//     'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5',
-//   ],
-//   [
-//     'History of the Web',
-//     'https://webfoundation.org/about/vision/history-of-the-web/',
-//   ],
-//   [
-//     'History of the Web (YouTube video)',
-//     'https://www.youtube.com/watch?v=h8K49dD52WA',
-//   ],
-//   [
-//     'Invention of Internet',
-//     'https://www.history.com/topics/inventions/invention-of-the-internet',
-//   ],
-//   [
-//     'How CSS works?',
-//     'https://developer.mozilla.org/en-US/docs/Learn/CSS/Introduction_to_CSS/How_CSS_works',
-//   ],
-//   ['Intro to CSS', 'https://developer.mozilla.org/en-US/docs/Web/CSS'],
-//   ['Intro to CSS 3', 'https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3'],
-//   [
-//     'Intro to Document Object Model (DOM)',
-//     'https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Introduction',
-//   ],
-//   [
-//     "The Difference Between 'Block' and 'Inline'",
-//     'http://www.impressivewebs.com/difference-block-inline-css/',
-//   ],
-//   [
-//     'MDN: Block-level elements',
-//     'https://developer.mozilla.org/en/docs/Web/HTML/Block-level_elements',
-//   ],
-//   [
-//     'MDN: Inline elements',
-//     'https://developer.mozilla.org/en-US/docs/Web/HTML/Inline_elements',
-//   ],
-// ];
+const resources = [
+  [
+    'An overview of HTTP',
+    'https://developer.mozilla.org/en-US/docs/Web/HTTP/Overview',
+  ],
+  [
+    'Visual Studio Code, Getting Started ',
+    'https://code.visualstudio.com/docs',
+  ],
+  [
+    'What is Pseudocode? How to Use Pseudocode to Solve Coding Problems',
+    'https://www.freecodecamp.org/news/what-is-pseudocode-in-programming',
+  ],
+  [
+    'History of the Web (YouTube video)',
+    'https://www.youtube.com/watch?v=h8K49dD52WA',
+  ],
+  [
+    'Invention of Internet',
+    'https://www.history.com/topics/inventions/invention-of-the-internet',
+  ],
+  [
+    'MDN: JavaScript data types and data structures',
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures',
+  ],
+  [
+    'MDN: Expressions and operators',
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators',
+  ],
+  [
+    'MDN: Arithmetic operators',
+    'https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators',
+  ],
+  [
+    'MDN: Working with objects',
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects',
+  ],
+  [
+    'Tuts+: What is type coercion?',
+    'http://code.tutsplus.com/articles/the-beginners-guide-to-type-coercion-what-is-coercion--cms-21917',
+  ],
+  [
+    'MDN: JavaScript Introduction',
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction',
+  ],
+  [
+    'MDN: Learning JavaScript',
+    'https://developer.mozilla.org/en-US/Learn/JavaScript',
+  ],
+  [
+    'Stack Overflow: When should I use a semicolon after curly braces?',
+    'http://stackoverflow.com/questions/2717949/when-should-i-use-a-semicolon-after-curly-braces',
+  ],
+];
 
-// const bonuses = [
-//   [
-//     "Complete a challenge that you haven't done yet on",
-//     'https://learn.shayhowe.com/html-css/getting-to-know-html/',
-//   ],
-
-//   [
-//     'Read through Getting to know CSS',
-//     'https://learn.shayhowe.com/html-css/getting-to-know-css/',
-//   ],
-// ];
+const bonuses = [
+  [
+    'Watch Intro to GitHub video',
+    'https://www.youtube.com/watch?v=vDv5K5PbvO8',
+  ],
+  [
+    "Complete Code Academy's Learn Git course",
+    'https://www.codecademy.com/learn/learn-git',
+  ],
+];
 
 const weekConfig = {
   weekNumber: 1,
-  // resources: resources,
-  // bonuses: bonuses,
+  resources,
+  bonuses,
   firstLessonTitle: 'Installfest',
   secondLessonTitle: 'The Command Line & Data Types',
-  // homeworkPath: '/week-1/lesson-2#43',
+  homeworkPath:
+    'https://github.com/pataruco/jsd/raw/master/homeworks/week-1/week-1-starter-code.zip',
 };
 
 const Week1 = () => <Week {...weekConfig} />;
