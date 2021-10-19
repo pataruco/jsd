a16cb9140823f965209f9f6e4451907e74bade6d
Pedro Martin <pataruco@gmail.com>
2021-10-19 21:17:36 +0100

    Enable week 2

---
+++ web/public/slides/02-collections-loops.md
@@ -0,0 +1,9 @@
+class: frontpage
+
+<div>
+  <h2>JavaScript Development</h2>
+  <hr/>
+  <h1>02 Collections and loops</h1>
+</div>
+
+---
--- web/src/routes/config.tsx
+++ web/src/routes/config.tsx
@@ -1,212 +1,69 @@
-import React, { lazy, Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import SlidesDeck from '../components/slides-deck';
 import Title from '../components/title';
 import Loading from '../pages/loading';
 
 // Lessons
 // Path: week-n/lesson-n
-
 export const lessonRoutes = [
   {
-    path: '/week-1/lesson-1',
+    path: '/week-1/lesson-0',
     component: () => (
       <>
-        <Title week={1} lesson={1} />
+        <Title week={1} lesson={0} />
         <SlidesDeck slidesDeckName="00-installfest" />
       </>
     ),
   },
   {
-    path: '/week-1/lesson-2',
+    path: '/week-1/lesson-1',
     component: () => (
       <>
-        <Title week={1} lesson={2} />
+        <Title week={1} lesson={1} />
         <SlidesDeck slidesDeckName="01-the-command-line-data-types" />
       </>
     ),
   },
-  // {
-  //   path: '/week-2/lesson-3',
-  //   component: () => (
-  //     <>
-  //       <Title week={2} lesson={3} />
-  //       <SlidesDeck slidesDeckName="03-box-model" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-2/lesson-4',
-  //   component: () => (
-  //     <>
-  //       <Title week={2} lesson={4} />
-  //       <SlidesDeck slidesDeckName="04-css-selectors-specificity-float" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-3/lesson-5',
-  //   component: () => (
-  //     <>
-  //       <Title week={3} lesson={5} />
-  //       <SlidesDeck slidesDeckName="05-flexbox" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-3/lesson-6',
-  //   component: () => (
-  //     <>
-  //       <Title week={3} lesson={6} />
-  //       <SlidesDeck slidesDeckName="06-positioning" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-4/lesson-7',
-  //   component: () => (
-  //     <>
-  //       <Title week={4} lesson={7} />
-  //       <SlidesDeck slidesDeckName="07-responsive" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-4/lesson-8',
-  //   component: () => (
-  //     <>
-  //       <Title week={4} lesson={8} />
-  //       <SlidesDeck slidesDeckName="08-responsive-lab" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-5/lesson-9',
-  //   component: () => (
-  //     <>
-  //       <Title week={5} lesson={9} />
-  //       <SlidesDeck slidesDeckName="09-intro-to-js" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-5/lesson-10',
-  //   component: () => (
-  //     <>
-  //       <Title week={5} lesson={10} />
-  //       <SlidesDeck slidesDeckName="10-document-object-model" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-6/lesson-11',
-  //   component: () => (
-  //     <>
-  //       <Title week={6} lesson={11} />
-  //       <SlidesDeck slidesDeckName="11-conditional-statements" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-6/lesson-12',
-  //   component: () => (
-  //     <>
-  //       <Title week={6} lesson={12} />
-  //       <SlidesDeck slidesDeckName="12-arrays-loops" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-7/lesson-13',
-  //   component: () => (
-  //     <>
-  //       <Title week={7} lesson={13} />
-  //       <SlidesDeck slidesDeckName="13-animations" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-7/lesson-14',
-  //   component: () => (
-  //     <>
-  //       <Title week={7} lesson={14} />
-  //       <SlidesDeck slidesDeckName="14-plugins" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-8/lesson-15',
-  //   component: () => (
-  //     <>
-  //       <Title week={8} lesson={15} />
-  //       <SlidesDeck slidesDeckName="15-forms" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-8/lesson-16',
-  //   component: () => (
-  //     <>
-  //       <Title week={8} lesson={16} />
-  //       <SlidesDeck slidesDeckName="16-api" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-9/lesson-17',
-  //   component: () => (
-  //     <>
-  //       <Title week={9} lesson={17} />
-  //       <SlidesDeck slidesDeckName="17-git-sass" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-9/lesson-18',
-  //   component: () => (
-  //     <>
-  //       <Title week={9} lesson={18} />
-  //       <SlidesDeck slidesDeckName="18-a11y-svg" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-10/lesson-19',
-  //   component: () => (
-  //     <>
-  //       <Title week={10} lesson={19} />
-  //       <SlidesDeck slidesDeckName="19-bootstrap" />
-  //     </>
-  //   ),
-  // },
-  // {
-  //   path: '/week-10/lesson-20',
-  //   component: () => (
-  //     <>
-  //       <Title week={10} lesson={20} />
-  //       <SlidesDeck slidesDeckName="20-final-presentations" />
-  //     </>
-  //   ),
-  // },
+  {
+    path: '/week-2/lesson-2',
+    component: () => (
+      <>
+        <Title week={2} lesson={2} />
+        <SlidesDeck slidesDeckName="02-collections-loops" />
+      </>
+    ),
+  },
+  {
+    path: '/week-2/lesson-3',
+    component: () => (
+      <>
+        <Title week={2} lesson={3} />
+        <SlidesDeck slidesDeckName="03-conditionals-functions" />
+      </>
+    ),
+  },
 ];
 
 // Weeks
 // Path: week-n
-export const weekRoutes = [
-  {
-    path: '/week-1',
-    component: () => {
-      const Content = lazy(() => import('../pages/weeks/1'));
-      return (
-        <>
-          <Title week={1} />
-          <Suspense fallback={<Loading />}>
-            <Content />
-          </Suspense>
-        </>
-      );
-    },
+export const weeks = new Array(2)
+  .fill(1) // Set number of weeks
+  .map((item, i) => (i === 0 ? 1 : item + i));
+
+export const weekRoutes = weeks.map((weekNumber) => ({
+  path: `/week-${weekNumber}`,
+  component: () => {
+    const Content = lazy(() => import(`../pages/weeks/${weekNumber}`));
+    return (
+      <>
+        <Title week={1} />
+        <Suspense fallback={<Loading />}>
+          <Content />
+        </Suspense>
+      </>
+    );
   },
-];
+}));
 
 // Bonus lessons
 // Path: /bonus-lessons/n
@@ -233,7 +90,6 @@ export const bonusLessonRoutes = [
 
 // Final project
 // Path: /final-project-brief
-
 export const FinalProjectRoute = {
   path: '/final-project-brief',
   component: () => {
