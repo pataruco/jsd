f8a08652f0cea2f1e034643110497239528c639b
Pedro Martin <pataruco@gmail.com>
2021-10-25 20:19:32 +0100

    Finish 99 bottles

--- homeworks/week-2/solution-code/99-bottles-of-beer/index.js
+++ homeworks/week-2/solution-code/99-bottles-of-beer/index.js
@@ -16,3 +16,39 @@
 */
 
 'use strict';
+
+const printNParagraph = (numberOfBottles) =>
+  console.log(`
+${numberOfBottles} bottles of beer on the wall, ${numberOfBottles} bottles of beer.
+Take one down and pass it around, ${
+    numberOfBottles - 1
+  } bottles of beer on the wall.
+`);
+
+const printOneBottleParagraph = () =>
+  console.log(`1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+`);
+
+const printZeroBottlesParagraph = () =>
+  console.log(`No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+`);
+
+let bottleCount = 100;
+
+do {
+  bottleCount--;
+
+  switch (bottleCount) {
+    case 1:
+      printOneBottleParagraph();
+      break;
+    case 0:
+      printZeroBottlesParagraph();
+      break;
+    default:
+      printNParagraph(bottleCount);
+      break;
+  }
+} while (bottleCount > 0);
