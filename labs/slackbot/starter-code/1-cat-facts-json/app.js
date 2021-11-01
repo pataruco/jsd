141cbbe1a52839f2fcee2ca4df9f05a768e59fc1
Pedro Martin <pataruco@gmail.com>
2021-11-01 21:10:22 +0000

    Draft slackbot lab instructions

---
+++ labs/slackbot/starter-code/1-cat-facts-json/app.js
@@ -0,0 +1,11 @@
+'use strict';
+// WEBSITE - https://cat-fact.herokuapp.com/#/cat/facts
+// API - https://cat-fact.herokuapp.com/facts
+
+// 1. Get the list of cat facts from the API
+
+// 2. Get first 10 facts from API
+
+// 3. Console log the first cat fact
+
+// 4. Console log the second user name that submitted the fact
