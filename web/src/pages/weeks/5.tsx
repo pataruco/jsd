06ea2a9c672c6b1eb388581c17191119348462ed
Pedro Martin <pataruco@gmail.com>
2021-11-22 21:01:34 +0000

    Add resources and bonuses

--- web/src/pages/weeks/5.tsx
+++ web/src/pages/weeks/5.tsx
@@ -1,46 +1,38 @@
 import Week from '../../components/week';
 
-// const resources = [
-//   [
-//     'MDN: JavaScript Introduction',
-//     'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction',
-//   ],
-//   [
-//     'MDN: Learning JavaScript',
-//     'https://developer.mozilla.org/en-US/Learn/JavaScript',
-//   ],
-//   [
-//     'Stack Overflow: When should I use a semicolon after curly braces?',
-//     'http://stackoverflow.com/questions/2717949/when-should-i-use-a-semicolon-after-curly-braces',
-//   ],
-//   [
-//     'MDN: Document',
-//     'https://developer.mozilla.org/en-US/docs/Web/API/Document',
-//   ],
-//   ['MDN: Window', 'https://developer.mozilla.org/en-US/docs/Web/API/Window'],
-//   ['MDN: Web APIs', 'https://developer.mozilla.org/en-US/docs/Web/API'],
-//   [
-//     'MDN: Event object',
-//     'https://developer.mozilla.org/en-US/docs/Web/API/Event',
-//   ],
-// ];
+const resources = [
+  [
+    'MDN: Sending form data',
+    'https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Sending_and_retrieving_form_data',
+  ],
+  [
+    'MDN: Using Fetch',
+    'https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch',
+  ],
+  [
+    'MDN: Using Promises',
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises',
+  ],
+  [
+    'MDN: try...catch',
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch',
+  ],
+];
 
-// const bonuses = [
-//   [
-//     'Read through MDN JavaScript Basics',
-//     'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics',
-//   ],
-//   [
-//     "Start Code Academy's Introduction to JavaScript",
-//     'https://www.codecademy.com/learn/introduction-to-javascript',
-//   ],
-// ];
+const bonuses = [
+  [
+    'We have a problem with promises ',
+    'https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html',
+  ],
+];
 
 const weekConfig = {
+  bonuses,
   firstLessonTitle: 'Asynchronous programming',
+  homeworkPath: '/week-5/lesson-9#7',
+  resources,
   secondLessonTitle: 'TfL API Lab 🚇 🧪',
   weekNumber: 5,
-  homeworkPath: '/week-5/lesson-9#7',
 };
 
 const Week5 = () => <Week {...weekConfig} />;
