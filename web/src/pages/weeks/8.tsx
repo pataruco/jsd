5d7b5d06433e24a4123d5cd2d33a7e6b62759af0
Pedro Martin <pataruco@gmail.com>
2022-01-02 16:18:23 +0000

    Finish startern code for prototypical inheritance

--- web/src/pages/weeks/8.tsx
+++ web/src/pages/weeks/8.tsx
@@ -1,57 +1,19 @@
 import Week from '../../components/week';
 
-// const resources = [
-//   [
-//     'MDN: Input',
-//     'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input',
-//   ],
-//   [
-//     'HTML5 Doctor: Input Types',
-//     'http://html5doctor.com/html5-forms-input-types/',
-//   ],
-//   ['Input Types', 'https://robertnyman.com/html5/forms/input-types.html'],
-//   [
-//     'MDN: Textarea',
-//     'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/textarea',
-//   ],
-//   [
-//     'CSS Tricks: Creating great web forms',
-//     'https://css-tricks.com/tips-for-creating-great-web-forms/',
-//   ],
-//   [
-//     'MDN: Sending form data',
-//     'https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Sending_and_retrieving_form_data',
-//   ],
-//   [
-//     'HTML Goodies: So you want to build a form?',
-//     'http://www.htmlgoodies.com/tutorials/forms/article.php/3479121',
-//   ],
-// ];
-
-// const bonuses = [
-//   ['Try a challenge on Frontend Mentor', 'https://www.frontendmentor.io'],
-//   [
-//     "Read Shay Howe's Building Forms",
-//     'http://learn.shayhowe.com/html-css/building-forms/',
-//   ],
-//   [
-//     "Read MDN's Your First HTML Form article",
-//     'https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Your_first_HTML_form',
-//   ],
-//   [
-//     'Read through Writing your best code',
-//     'http://learn.shayhowe.com/html-css/writing-your-best-code/',
-//   ],
-//   [
-//     "Complete Code Academy's  Make a website course",
-//     'https://www.codecademy.com/learn/make-a-website',
-//   ],
-// ];
+const resources = [
+  [
+    'MDN: for...in',
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in',
+  ],
+  [
+    'MDN: prototype call ',
+    'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call',
+  ],
+];
 
 const weekConfig = {
-  // bonuses,
   firstLessonTitle: 'Prototypal Inheritance',
-  // resources,
+  resources,
   secondLessonTitle: 'Closures & this',
   weekNumber: 8,
 };
